0aaac638c9fc5d58706a20a05d361659bef57e42
Elian Frentzel <elian.y.frentzel@gmail.com>
2024-10-19 16:21:31 +0200

    add template to config

--- Device.py
+++ Device.py
@@ -2,6 +2,7 @@ import json
 import pathlib
 import subprocess
 import time
+import socket
 
 from adb_shell.adb_device import AdbDeviceTcp, AdbDeviceUsb
 from adb_shell.exceptions import UsbDeviceNotFoundError
@@ -65,15 +66,39 @@ class Device:
 
 
     def loadConfig(self):
-        remotePath = pathlib.Path('/sdcard', 'MediaMove', 'config.json')
-        localPath = pathlib.Path('.tmp', f'{self.serialno}_config.json')
+        template = {
+            "uniqueLocal": "copy/move/delete/ignore",
+            "newLocal": "updateLocal/updateRemote/ignore",
+            "newRemote": "updateLocal/updateRemote/ignore",
+            "uniqueRemote": "copy/move/delete/ignore",
+            "local": "",
+            "remote": "",
+            "excl": []
+        }
+        
+        name = socket.gethostname()
+        remotePath = pathlib.Path('/sdcard', 'MediaMove', f'{name}.json')
+        localPath = pathlib.Path('.tmp', f'{self.serialno}_{name}.json')
 
-        self().pull(str(remotePath), localPath)
+        if 1 == self().shell(f'test -e {remotePath.parent}; echo $?'):
+            self().shell(f'mkdir {remotePath.parent}')
+        
+        if 1 == self().shell(f'test -e {remotePath}; echo $?'):
+            localPath.write_text(json.dumps([template], indent=4))
+        else:
+            if not localPath.exists():
+                self().pull(str(remotePath), localPath)
+
+            content = json.loads(localPath.read_bytes())
+            if template not in content:
+                content.append(template)
+                localPath.write_text(json.dumps(content, indent=4))
 
 
     def saveConfig(self):
-        remotePath = pathlib.Path('/sdcard', 'MediaMove', 'config.json')
-        localPath = pathlib.Path('.tmp', f'{self.serialno}_config.json')
+        name = socket.gethostname()
+        remotePath = pathlib.Path('/sdcard', 'MediaMove', f'{name}.json')
+        localPath = pathlib.Path('.tmp', f'{self.serialno}_{name}.json')
 
         content = json.loads(localPath.read_bytes())
         localPath.write_text(json.dumps(content, indent=4))
